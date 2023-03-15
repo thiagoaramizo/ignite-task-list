d4c721a2011aa988cee3a9781c95d6883861bda4
Thiago Aramizo Ribeiro <aramizot@users.noreply.github.com>
2023-03-15 01:58:58 -0300

    entrega

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle)
+    {
+      const newTask: Task = {
+        id: Math.floor(Date.now() * Math.random()),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks( [ ...tasks, newTask] )
+      setNewTaskTitle( '' )
+    }    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks( tasks.map( t => {
+      if( t.id === id ){
+        t.isComplete = !t.isComplete
+      } 
+      return t
+    } ) )
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks( tasks.filter( t => t.id !== id ) )
   }
 
   return (
